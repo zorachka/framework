3ea74598e4c0de21f8225ce6d945afba351f8b4a
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-07-01 09:47:19 +0400

    feat: add Application contract

---
+++ src/Contracts/Application.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zorachka\Contracts;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface Application
+{
+    /**
+     * @param ServerRequestInterface|null $request
+     */
+    public function run(?ServerRequestInterface $request = null): void;
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface;
+
+    /**
+     * @return ContainerInterface
+     */
+    public function container(): ContainerInterface;
+}
