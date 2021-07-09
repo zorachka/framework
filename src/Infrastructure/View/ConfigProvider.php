5967fae738c2d54b8615435a1bca6d95e1a81bf3
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-07-09 14:41:04 +0400

    fix: Response factory interface

--- src/Infrastructure/View/ConfigProvider.php
+++ src/Infrastructure/View/ConfigProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Zorachka\Infrastructure\View;
 
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
+use Zorachka\Application\Http\ResponseFactory;
 use Zorachka\Application\Templer\Templer;
 use Zorachka\Application\View\View;
 
@@ -15,8 +15,8 @@ final class ConfigProvider
     {
         return [
             View::class => static function (ContainerInterface $container) {
-                /** @var ResponseFactoryInterface $responseFactory */
-                $responseFactory = $container->get(ResponseFactoryInterface::class);
+                /** @var ResponseFactory $responseFactory */
+                $responseFactory = $container->get(ResponseFactory::class);
                 /** @var Templer $templer */
                 $templer = $container->get(Templer::class);
 
