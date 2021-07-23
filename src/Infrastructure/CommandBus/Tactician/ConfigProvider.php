1ce8a7c36a97e03e89a1907269f01406d04ef46e
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-07-23 10:42:14 +0400

    fix: Tactician Command Bus

--- src/Infrastructure/CommandBus/Tactician/ConfigProvider.php
+++ src/Infrastructure/CommandBus/Tactician/ConfigProvider.php
@@ -21,7 +21,7 @@ final class ConfigProvider
 {
     public function __invoke(): array
     {
-        $defaultConfig = Config::defaults();
+        $defaultConfig = Config::withDefaults();
         $defaults = $defaultConfig();
 
         return [
